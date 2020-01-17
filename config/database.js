39616b85df4d04883b3326bc64dea5f7c074a32c
kitu <deepakgantayat29@gmaim>
2020-01-16 19:03:50 -0800

    featured: nw Commit

--- config/database.js
+++ config/database.js
@@ -2,7 +2,9 @@ const mongoose = require('mongoose')
 
 // db configuration
 const configureDB =  () => {
-    mongoose.connect('mongodb://localhost:27017/aug-notes-app', { useNewUrlParser: true, useUnifiedTopology: true})
+    mongoose.Promise = global.Promise
+    const MONGODB_URI = process.emv.MONGODB_URI || 'mongodb://localhost:27017/aug-notes-app'
+    mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true})
         .then(() => {
             console.log('Connected to database')
         })
